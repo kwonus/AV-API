42455a72b260fcd7a9207b8c887603234e16a087
Kevin Wonus <kevin@wonus.com>
2024-07-08 18:51:18 -0400

    Converted console app into a loadable library, compatible with WPF hosting

--- API.cs
+++ API.cs
@@ -8,168 +8,166 @@ using Pinshot.Blue;
 using System;
 using System.IO;
 using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
 using static Blueprint.Model.Implicit.QFormat;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace AVAPI
 {
-    public class API
+    public class API: HostedWebServer
     {
         private static API api;
         internal Engine engine;
-        internal static Guid ID;
-        internal static WebApplication App;
+
         static API()
         {
             API.api = new();
         }
-        private API()
+        public API()
         {
             this.engine = new(API.api);
             QContext.Home = Directory.GetCurrentDirectory();
         }
-        public static void Start()
-        {
-            API.ID = Guid.NewGuid();
-        }
-        public static void Stop(Guid id)
-        {
-            if (id == API.ID)
-            {
-                API.App.StopAsync();
-            }
-        }
-        public static string Heartbeat()
-        {
-            return API.ID.ToString();
-        }
-        public static void Main(string[] args)
-        {
-            API.Start();
 
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        protected override bool Launch(string url)
+        {
             string yaml = "text/x-yaml; charset=utf-8";
             string text = "text/plain; charset=utf-8";
             string html = "text/html; charset=utf-8";
             string mkdn = "text/markdown; charset=utf-8";
 
-            // NOTE:
-            // Unlike inproc library, the API has not concept of non-persistent settings.
-            // If a setting is passed in, it is explictly saved.
-
             string message = string.Empty;
 
-            var builder = WebApplication.CreateBuilder(args);
-            API.App = builder.Build();
-
-            API.App.MapGet("/exit", (Guid id) => API.Stop(id));
-            API.App.MapGet("/status", () => API.Heartbeat());
-
-            API.App.MapGet("/settings.yml", () => API.api.engine.Get_Settings());
-
-            API.App.MapGet("/settings/span",    (uint value)   => API.api.engine.Update_Settings("span", value.ToString()));
-            API.App.MapGet("/settings/word",    (string value) => API.api.engine.Update_Settings("word", value));
-            API.App.MapGet("/settings/lemma",   (string value) => API.api.engine.Update_Settings("lemma", value));
-            API.App.MapGet("/settings/lexicon", (string value) => API.api.engine.Update_Settings("lexicon", value));
-            API.App.MapGet("/settings/search",  (string value) => API.api.engine.Update_Settings("lexicon", value));
-            API.App.MapGet("/settings/render",  (string value) => API.api.engine.Update_Settings("render", value));
-            API.App.MapGet("/settings/format",  (string value) => API.api.engine.Update_Settings("format", value));
-
-            API.App.MapGet("/debug/find/{spec}", (string spec) => API.api.engine.Debug_Find(spec, out message, quoted: false).ToString());
-            API.App.MapGet("/debug/find-quoted/{spec}", (string spec) => API.api.engine.Debug_Find(spec, out message, quoted: true).ToString());
-
-            string binary = "application/octet-stream";
-            API.App.MapGet("/find/{spec}", (string spec) => Results.Stream(API.api.engine.Binary_Find(spec, out message, quoted: false), binary));
-            API.App.MapGet("/find-quoted/{spec}", (string spec) => Results.Stream(API.api.engine.Binary_Find(spec, out message, quoted: true), binary));
-
-            API.App.MapGet("/{book}/{chapter}.yml", (string book, string chapter)
-                => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.YAML), book, chapter, out message), yaml));
-            API.App.MapGet("/{book}/{chapter}/find/{spec}.yml", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message), yaml));
-            API.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.yml", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, quoted: true), yaml));
-
-            API.App.MapGet("/{book}/{chapter}.txt", (string book, string chapter)
-                => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.TEXT), book, chapter, out message), text));
-            API.App.MapGet("/{book}/{chapter}/find/{spec}.txt", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message), text));
-            API.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.txt", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, quoted: true), text));
-
-            API.App.MapGet("/{book}/{chapter}.html", (string book, string chapter)
-                => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.HTML), book, chapter, out message), html));
-            API.App.MapGet("/{book}/{chapter}/find/{spec}.html", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message), html));
-            API.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.html", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, quoted: true), html));
-
-            API.App.MapGet("/{book}/{chapter}/.md", (string book, string chapter)
-                => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.MD), book, chapter, out message), mkdn));
-            API.App.MapGet("/{book}/{chapter}/find/{spec}.md", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message), mkdn));
-            API.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.md", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, quoted: true), mkdn));
-
-            API.App.MapGet("/{book}/{chapter}/context-find/{spec}.yml", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, context: true), yaml));
-            API.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.yml", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, context: true, quoted: true), yaml));
-
-            API.App.MapGet("/{book}/{chapter}/context-find/{spec}.txt", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, context: true), text));
-            API.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.txt", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, context: true, quoted: true), text));
-
-            API.App.MapGet("/{book}/{chapter}/context-find/{spec}.html", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, context: true), html));
-            API.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.html", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, context: true, quoted: true), html));
-
-            API.App.MapGet("/{book}/{chapter}/context-find/{spec}.md", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, context: true), mkdn));
-            API.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.md", (string book, string chapter, string spec)
-                => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, context: true, quoted: true), mkdn));
-
-
-            API.App.MapGet("/{book}/{chapter}/", (string book, string chapter) => $"unhighlighted {book}:{chapter}");
-            API.App.MapGet("/help/diagrams/{image}.png", (string image) =>
+            try
             {
-                var path = Path.Combine(AVEngine.HelpFolder, "diagrams", $"{image}.png");
-                return Results.File(path, contentType: "image/png");
-            });
-            API.App.MapGet("/help/css/{style}.css", (string style) =>
-            {
-                var path = Path.Combine(AVEngine.HelpFolder, "css", $"{style}.css");
-                return Results.File(path, contentType: "text/css");
-            });
-            API.App.MapGet("/help/html-generator/{js}.js", (string js) =>
-            {
-                var path = Path.Combine(AVEngine.HelpFolder, "html-generator", $"{js}.js");
-                return Results.File(path, contentType: "text/javascript");
-            });
-            API.App.MapGet("/help/{help}.html", (string help) =>
-            {
-                var path = Path.Combine(AVEngine.HelpFolder, $"{help}.html");
-                return Results.File(path, contentType: "text/html");
-            });
-            API.App.MapGet("/help/{help}", (string help) =>
-            {
-                var path = AVEngine.GetHelpFile(help);
-                return Results.File(path, contentType: "text/html");
-            });
-            API.App.MapGet("/help", () =>
-            {
-                var path = AVEngine.GetHelpFile("index.html");
-                return Results.File(path, contentType: "text/html");
-            });
-            API.App.MapGet("/", () => "Hello AV-Bible user!\nAV-Engine Version: " + Pinshot_RustFFI.VERSION);
 
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                var builder = WebApplication.CreateBuilder();
+                this.App = builder.Build();
 
-#if DEBUG
-            API.App.Run("http://localhost:1769");
-#else
-            API.App.Run();
-#endif
+                this.App.MapGet("/settings.yml", () => API.api.engine.Get_Settings());
+
+                this.App.MapGet("/settings/span", (uint value) => API.api.engine.Update_Settings("span", value.ToString()));
+                this.App.MapGet("/settings/word", (string value) => API.api.engine.Update_Settings("word", value));
+                this.App.MapGet("/settings/lemma", (string value) => API.api.engine.Update_Settings("lemma", value));
+                this.App.MapGet("/settings/lexicon", (string value) => API.api.engine.Update_Settings("lexicon", value));
+                this.App.MapGet("/settings/search", (string value) => API.api.engine.Update_Settings("lexicon", value));
+                this.App.MapGet("/settings/render", (string value) => API.api.engine.Update_Settings("render", value));
+                this.App.MapGet("/settings/format", (string value) => API.api.engine.Update_Settings("format", value));
+
+                this.App.MapGet("/debug/find/{spec}", (string spec) => API.api.engine.Debug_Find(spec, out message, quoted: false).ToString());
+                this.App.MapGet("/debug/find-quoted/{spec}", (string spec) => API.api.engine.Debug_Find(spec, out message, quoted: true).ToString());
+
+                string binary = "application/octet-stream";
+                this.App.MapGet("/find/{spec}", (string spec) => Results.Stream(API.api.engine.Binary_Find(spec, out message, quoted: false), binary));
+                this.App.MapGet("/find-quoted/{spec}", (string spec) => Results.Stream(API.api.engine.Binary_Find(spec, out message, quoted: true), binary));
+
+                this.App.MapGet("/{book}/{chapter}.yml", (string book, string chapter)
+                    => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.YAML), book, chapter, out message), yaml));
+                this.App.MapGet("/{book}/{chapter}/find/{spec}.yml", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message), yaml));
+                this.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.yml", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, quoted: true), yaml));
+
+                this.App.MapGet("/{book}/{chapter}.txt", (string book, string chapter)
+                    => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.TEXT), book, chapter, out message), text));
+                this.App.MapGet("/{book}/{chapter}/find/{spec}.txt", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message), text));
+                this.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.txt", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, quoted: true), text));
+
+                this.App.MapGet("/{book}/{chapter}.html", (string book, string chapter)
+                    => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.HTML), book, chapter, out message), html));
+                this.App.MapGet("/{book}/{chapter}/find/{spec}.html", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message), html));
+                this.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.html", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, quoted: true), html));
+
+                this.App.MapGet("/{book}/{chapter}/.md", (string book, string chapter)
+                    => Results.Stream(API.api.engine.Get_Chapter(nameof(QFormatVal.MD), book, chapter, out message), mkdn));
+                this.App.MapGet("/{book}/{chapter}/find/{spec}.md", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message), mkdn));
+                this.App.MapGet("/{book}/{chapter}/find-quoted/{spec}.md", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, quoted: true), mkdn));
+
+                this.App.MapGet("/{book}/{chapter}/context-find/{spec}.yml", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, context: true), yaml));
+                this.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.yml", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.YAML), spec, book, chapter, out message, context: true, quoted: true), yaml));
+
+                this.App.MapGet("/{book}/{chapter}/context-find/{spec}.txt", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, context: true), text));
+                this.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.txt", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.TEXT), spec, book, chapter, out message, context: true, quoted: true), text));
+
+                this.App.MapGet("/{book}/{chapter}/context-find/{spec}.html", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, context: true), html));
+                this.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.html", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.HTML), spec, book, chapter, out message, context: true, quoted: true), html));
+
+                this.App.MapGet("/{book}/{chapter}/context-find/{spec}.md", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, context: true), mkdn));
+                this.App.MapGet("/{book}/{chapter}/context-find-quoted/{spec}.md", (string book, string chapter, string spec)
+                    => Results.Stream(API.api.engine.Detail_Find(nameof(QFormatVal.MD), spec, book, chapter, out message, context: true, quoted: true), mkdn));
+
+
+                this.App.MapGet("/{book}/{chapter}/", (string book, string chapter) => $"unhighlighted {book}:{chapter}");
+                this.App.MapGet("/help/diagrams/{image}.png", (string image) =>
+                {
+                    var path = Path.Combine(AVEngine.HelpFolder, "diagrams", $"{image}.png");
+                    return Results.File(path, contentType: "image/png");
+                });
+                this.App.MapGet("/help/css/{style}.css", (string style) =>
+                {
+                    var path = Path.Combine(AVEngine.HelpFolder, "css", $"{style}.css");
+                    return Results.File(path, contentType: "text/css");
+                });
+                this.App.MapGet("/help/html-generator/{js}.js", (string js) =>
+                {
+                    var path = Path.Combine(AVEngine.HelpFolder, "html-generator", $"{js}.js");
+                    return Results.File(path, contentType: "text/javascript");
+                });
+                this.App.MapGet("/help/{help}.html", (string help) =>
+                {
+                    var path = Path.Combine(AVEngine.HelpFolder, $"{help}.html");
+                    return Results.File(path, contentType: "text/html");
+                });
+                this.App.MapGet("/help/{help}", (string help) =>
+                {
+                    var path = AVEngine.GetHelpFile(help);
+                    return Results.File(path, contentType: "text/html");
+                });
+                this.App.MapGet("/help", () =>
+                {
+                    var path = AVEngine.GetHelpFile("index.html");
+                    return Results.File(path, contentType: "text/html");
+                });
+                this.App.MapGet("/", () => "Hello AV-Bible user!\nAV-Engine Version: " + Pinshot_RustFFI.VERSION);
+
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                this.App.Run("http://localhost:1769");
+                this.IsRunning = true;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.LastException = ex;
+                this.ErrorMessage = ex.Message;
+                this.IsRunning = false;
+            }
+            return false;
         }
     }
 }
---
+++ HostedWebServer.cs
@@ -0,0 +1,86 @@
+﻿// Adapted from: https://github.com/RickStrahl/Westwind.AspNetCore.HostedWebServer
+
+namespace AVAPI
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.FileProviders;
+
+    public abstract class HostedWebServer
+    {
+        public bool IsRunning { get; set; }
+
+        public WebApplication App { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public Exception LastException { get; set; }
+
+        public Action<WebApplication> OnMapRequests { get; set; }
+
+        public Action<HttpContext> OnRequestStarting { get; set; }
+
+        public Action<HttpContext, TimeSpan> OnRequestCompleted { get; set; }
+
+        /// <summary>
+        /// Launches the Web Server synchronously and
+        /// waits for it to shut down
+        /// </summary>
+        /// <param name="url">startup url</param>
+        /// <param name="webRootPath">An optional path for static file locations. If not specified uses entry assembly location</param>
+        /// <returns></returns>
+        protected abstract bool Launch(string url);
+
+        /// <summary>
+        /// Launches the Web Server in the background so you can continue
+        /// processing
+        /// so you 
+        /// </summary>
+        /// <param name="url">tartup url</param>
+        /// <param name="webRootPath"></param>
+        public async Task<bool> LaunchAsync(string url = "http://localhost:1769")
+        {
+            return await Task.Run(() => Launch(url));
+        }
+
+
+        public async Task Stop()
+        {
+            await App.StopAsync();
+            IsRunning = false;
+        }
+
+        /// <summary>
+        /// This middle ware handler intercepts every request captures the time
+        /// and then logs out to the screen (when that feature is enabled) the active
+        /// request path, the status, processing time.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        async Task RequestInterceptorMiddlewareHandler(HttpContext context, Func<Task> next)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            if (OnRequestStarting != null)
+            {
+                OnRequestStarting.Invoke(context);
+            }
+
+            await next();
+
+            if (OnRequestCompleted != null)
+            {
+                OnRequestCompleted.Invoke(context, sw.Elapsed);
+            }
+        }
+    }
+}
+
